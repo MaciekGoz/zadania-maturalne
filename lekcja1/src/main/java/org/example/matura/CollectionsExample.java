f302a346fa2b204f78a0b9fb0bd65e27775042b6
PanMurzyn <maciek.gozdalik@gmail.com>
2020-10-13 17:05:18 +0200

    1, 2, 3

---
+++ lekcja1/src/main/java/org/example/matura/CollectionsExample.java
@@ -0,0 +1,43 @@
+package org.example.matura;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CollectionsExample {
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            int random = ThreadLocalRandom.current().nextInt(100);
+            integers.add(random);
+        }
+
+        System.out.println(listContains(integers, 50));
+
+        System.out.println(integers);
+
+        if(integers.contains(50)) {
+            System.out.println("lista zawiera 50");
+        } else {
+            System.out.println("lista nie zawiera 50");
+        }
+
+        Stack<String> stack = new Stack<>();
+        stack.push("ala");
+        stack.push("ma");
+        stack.push("kota");
+
+        System.out.println(stack.peek());
+    }
+
+    public static boolean listContains(List<Integer> list, int numberToFind) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == numberToFind) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
